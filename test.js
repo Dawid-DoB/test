b36b9a7067a8aa0b61495b20e016aebeabe58858
Dawid-DoB <dawid.kaszewski@dietyodbrokula.pl>
2025-11-07 15:45:30 +0100

    Update test.js

--- test.js
+++ test.js
@@ -3,7 +3,7 @@
       spaceBetween: 0,
       centeredSlides: true,
       loop: true,
-      speed: 500,
+      speed: 1000,
       autoplay: {
         delay: 4000,
         disableOnInteraction: true,
