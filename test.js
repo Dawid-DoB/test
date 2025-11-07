306e0c83f40b77158b1b2b489822194e4d10a5b0
Dawid-DoB <dawid.kaszewski@dietyodbrokula.pl>
2025-11-07 11:56:23 +0100

    Create test.js

---
+++ test.js
@@ -0,0 +1,9 @@
+    var swiper = new Swiper("#swiper", {
+      slidesPerView: 6,
+      spaceBetween: 20,
+      centeredSlides: true,
+      pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+      },
+    });
